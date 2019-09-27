6a4e3a78a876d06f8dea8ec3927b3dc9403e2196
vivek87gupta <lrvivekgupta@gmail.com>
2019-09-27 11:37:39 +0530

    updated

--- src/helloworld/main_test.go
+++ src/helloworld/main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHelloWorld(t *testing.T) {
 	var msg = helloWorld()
-	if msg != "Hello World! vivek" {
+	if msg != "Hello World! vivek 1" {
 		t.Errorf("helloWorld did not work, expected Hello World! but got %s", msg)
 	}
 }
